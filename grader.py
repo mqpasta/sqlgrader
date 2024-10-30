2f3e33b53b64480ec7634585c7f585fa7fc94333
mqpasta <mqpasta@gmail.com>
2024-10-30 18:56:49 +0500

    create logs for errors

--- grader.py
+++ grader.py
@@ -2,6 +2,7 @@ import pyodbc
 import re
 import os
 import csv
+import sys
 
 def read_solution_quries(filename):
     solutions = {}
@@ -49,17 +50,23 @@ def extract_queries(file_path):
 def execute_and_grade(queries, correct_results, conn):
     cursor = conn.cursor()
     grades = {}
+    errors = {}
     for question, query in queries.items():
         try:
+            if len(query) == 0:
+                grades[question] = 'Not Attempted'
+                continue
             cursor.execute(query)
             student_result = cursor.fetchall()
             if student_result == correct_results.get(question):
                 grades[question] = 'Correct'
             else:
                 grades[question] = 'Incorrect'
+                errors[question] = f'Expected {correct_results.get(question)} \n got {student_result}'
         except Exception as e:
             grades[question] = f'Error'
-    return grades
+            errors[question] = f'Error: {e}'
+    return grades, errors
 
 # Function to write grades to a file
 def write_grades(student_name, student_id, grades, output_file='grades.txt'):
@@ -87,8 +94,13 @@ def process_all_submissions(directory, correct_answers, server, database):
             student_id = file_name.split('_')[-1]    
 
             student_queries = extract_queries(file_path)  # Extract student's SQL queries
-            grades = execute_and_grade(student_queries, correct_results, conn)  # Grade student submission
+            grades, errors = execute_and_grade(student_queries, correct_results, conn)  # Grade student submission
             write_grades(student_name, student_id, grades)  # Write grades to file
+            if len(errors) > 0:
+                with open("errors/"+file_name+".log", 'a') as file:
+                    for question, error in errors.items():
+                        file.write(f'{question}: {error}\n')
+                    file.write('\n')
 
     conn.close()
 
@@ -98,13 +110,23 @@ def generate_output_file(output_file='grades.txt', questions=['Q1', 'Q2']):
         file.write(','.join(questions))
         file.write('\n')
 
+def create_error_directory(directory):
+    if not os.path.exists(directory):
+        print(f"Creating directory {directory}")
+        os.makedirs(directory)
+
 # Example usage
 if __name__ == '__main__':
     directory = 'submissions'
     server = '.\SQLEXPRESS'
     database = 'eis'
     solution = 'solutions.csv'
+    error_directory = 'errors'
+
+    for index,arg in enumerate(sys.argv):
+        print(f"Argument {index}: {arg}")
     
+    create_error_directory(error_directory)
     correct_answers = read_solution_quries(solution)
     
     generate_output_file(questions=correct_answers.keys())
